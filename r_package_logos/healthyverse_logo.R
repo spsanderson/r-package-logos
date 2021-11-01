7b56d7203add1a0748c3f551d128a1ba310f3fc0
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-01 15:55:04 -0400

    update
    change healthyR.data logo, makek healthyverse logo

--- r_package_logos/healthyverse_logo.R
+++ r_package_logos/healthyverse_logo.R
@@ -4,28 +4,29 @@ library(showtext)
 
 font_add_google("Major Mono Display")
 
-data_tbl <- tibble(
-    x = 1:5,
-    y = 1:5
-)
+data_tbl <- data.frame(t = seq(0, 2 * pi, by = 0.01))
+xhrt <- function(t) 16 * sin(t) ^ 3
+yhrt <- function(t) 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)
+data_tbl$y <- yhrt(data_tbl$t)
+data_tbl$x <- xhrt(data_tbl$t)
 
 p <- data_tbl %>%
     ggplot(mapping = aes(x = x
                          , y = y)) +
-    geom_col(fill = "red", color = "red",size = 0.2) +
+    geom_path(color = "red",size = 0.2) +
     theme_void() +
     theme_transparent() +
     theme(legend.position = "none")
 
 sticker(
     p,
-    package = "{healthyR.data}",
-    filename = "C:/Users/Steve/Documents/GitHub/r-package-logos/r_package_logos/healthyr_data/test3.png",
+    package = "{healthyverse}",
+    filename = "C:/Users/Steve/Documents/GitHub/r-package-logos/r_package_logos/healthyverse/test2.png",
     p_size = 11,
     s_x = 1,
     s_y = .7,
     s_width = 1.4,
-    s_height = 0.6,
+    s_height = 1.2,
     p_family = "Major Mono Display",
     h_fill = "white",
     h_color = "steel blue",
