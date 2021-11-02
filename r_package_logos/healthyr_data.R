ee432700adb11255d5dfcafd61857b2e12bbc36b
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-02 14:06:58 -0400

    update healthyr data logo

--- r_package_logos/healthyr_data.R
+++ r_package_logos/healthyr_data.R
@@ -6,13 +6,13 @@ font_add_google("Major Mono Display")
 
 data_tbl <- tibble(
     x = 1:5,
-    y = 1:5
+    y = c(1,3,2,5,4)
 )
 
 p <- data_tbl %>%
     ggplot(mapping = aes(x = x
                          , y = y)) +
-    geom_col(fill = "red", color = "red",size = 0.2) +
+    geom_col(fill = "white", color = "red",size = 0.2) +
     theme_void() +
     theme_transparent() +
     theme(legend.position = "none")
@@ -20,12 +20,12 @@ p <- data_tbl %>%
 sticker(
     p,
     package = "{healthyR.data}",
-    filename = "C:/Users/Steve/Documents/GitHub/r-package-logos/r_package_logos/healthyr_data/test3.png",
+    filename = "C:/Users/Steve/Documents/GitHub/r-package-logos/r_package_logos/healthyr_data/test4.png",
     p_size = 11,
     s_x = 1,
-    s_y = .7,
+    s_y = .8,
     s_width = 1.4,
-    s_height = 0.6,
+    s_height = 0.5,
     p_family = "Major Mono Display",
     h_fill = "white",
     h_color = "steel blue",
